96d5b66b44dc1190425a6a925ac6ffa67284df44
henteko <henteko07@gmail.com>
2013-09-23 08:05:22 +0900

    #5 add ApkTarget

---
+++ src/main/groovy/com/deploygate/gradle/plugins/ApkTarget.groovy
@@ -0,0 +1,23 @@
+package com.deploygate.gradle.plugins
+
+import org.gradle.api.DomainObjectSet
+import org.gradle.api.Named
+import org.gradle.api.internal.project.ProjectInternal
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.TaskAction
+import org.gradle.plugins.binaries.model.NativeDependencyCapableSourceSet;
+import org.gradle.plugins.binaries.model.NativeDependencySet
+
+class ApkTarget implements Named {
+  String name
+  ProjectInternal target 
+  File sourceFile
+  String message
+  String distributionKey
+  String releaseNote
+  public ApkTarget(String name) {
+    super()
+    this.name = name
+    this.target = target
+  }
+}
