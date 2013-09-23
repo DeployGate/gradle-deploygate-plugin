0e4238b68cb0bcf72ecd566cbe256a11e87fa28b
henteko <henteko07@gmail.com>
2013-09-23 14:59:48 +0900

    #5 support multiple apks

--- src/main/groovy/com/deploygate/gradle/plugins/DeployGate.groovy
+++ src/main/groovy/com/deploygate/gradle/plugins/DeployGate.groovy
@@ -20,114 +20,85 @@ import org.json.JSONObject
 import java.nio.charset.Charset
 
 class DeployGate implements Plugin<Project> {
-    void apply(Project target) {
-        target.extensions.create('deploygate', DeployGateExtension)
-        def apkUpload = target.task('uploadDeployGate') << {
-            HashMap<String, String> params = setBaseParams(target)
-            JSONObject json = httpPost(params)
-            errorHandling(json) 
+    private final String API_END_POINT = "https://deploygate.com/api"
 
-            println json.toString() 
+    void apply(Project target) {
+
+        def deploygate = new DeployGateExtension(target.container(ApkTarget))
+        target.convention.plugins.deploygate = deploygate 
+        target.extensions.deploygate = deploygate
+
+        def apkUpload = target.task('uploadDeployGate') << {
+            List<Apk> apks = Apk.getApks(target)
+            String endPoint = getEndPoint(target)
+            String token = getToken(target)
+
+            HashMap<String, JSONObject> result = httpPost(endPoint, token, apks)
+            for(Apk apk in apks) {
+                JSONObject json = result.get(apk.name)
+                errorHandling(apk, json) 
+                println "${apk.name} result: ${json.toString()}"
+            }
         }
         apkUpload.group = 'DeployGate' 
         apkUpload.description = 'Upload the apk file to deploygate'
-
-        def distributionUpdate = target.task('updateDeployGateDistribution') << {
-            String distributionKey = target.deploygate.distributionKey
-            String releaseNote = target.deploygate.releaseNote
-            if(distributionKey == null || distributionKey == '') {
-                throw new GradleException('distributionKey is missing. Please enter the distributionKey.')
-            }
-            if(releaseNote == null) {
-                releaseNote = ''
-            }
-            
-            HashMap<String, String> params = setBaseParams(target)
-            params.put("distribution_key", distributionKey)
-            params.put("release_note", target.deploygate.releaseNote)
-
-            JSONObject json = httpPost(params)
-            errorHandling(json) 
-
-            println json.toString()
-        }
-        distributionUpdate.group = 'DeployGate' 
-        distributionUpdate.description = 'Apk upload and distribution update'
     }
 
-    private void errorHandling(JSONObject json) {
+    private void errorHandling(Apk apk, JSONObject json) {
         if(json['error'] == true) {
-            throw new GradleException('error massage: ' + json['message'])
+            throw new GradleException("${apk.name} error massage: ${json['message']}")
         }
     }
 
-    private HashMap<String, String> setBaseParams(Project target) {
-        String userName = target.deploygate.userName
-        String apkPath = target.deploygate.apkPath
+    private String getToken(Project target) {
         String token = target.deploygate.token
-        String message = target.deploygate.message
-        if(userName == null || userName == '') {
-            throw new GradleException('userName is missing. Please enter the userName.')
-        }
-        if(apkPath == null || apkPath == '') {
-            throw new GradleException('apkPath is missing. Please enter the apkPath.')
-        }
         if(token == null || token == '') {
             throw new GradleException('token is missing. Please enter the token.')
         }
-        if(message == null) {
-            message = ''
-        }
-
-        String endPoint = "https://deploygate.com/api/users/${userName}/apps"
-        HashMap<String, String> params = new HashMap<String, String>()
-        params.put("file", apkPath)
-        params.put("token", token)
-        params.put("message", target.deploygate.message)
-        params.put("endPoint", endPoint)
-        
-        return params
+        return token
     }
 
-    private JSONObject httpPost(HashMap<String, String> params) {
-        HttpClient httpclient = new DefaultHttpClient()
-        String endPoint = params.get("endPoint")
-        HttpPost httppost = new HttpPost(endPoint)
-        MultipartEntity request_entity = new MultipartEntity()
-        Charset charset = Charset.forName(HTTP.UTF_8)
-        
-        File file = new File(params.get("file"))
-        request_entity.addPart("file", new FileBody(file.getAbsoluteFile()))
-        params.remove("file")
-        params.remove("endPoint")
-
-        for (String key : params.keySet()) {
-            request_entity.addPart(key, new StringBody(params.get(key), charset))
+    private String getEndPoint(Project target) {
+        String userName = target.deploygate.userName
+        if(userName == null || userName == '') {
+            throw new GradleException('userName is missing. Please enter the userName.')
         }
+        String endPoint = API_END_POINT + "/users/${userName}/apps"
+        return endPoint
+    }
 
-        httppost.setEntity(request_entity)
-        HttpResponse response = httpclient.execute(httppost)
-        HttpEntity entity = response.getEntity()
+    private HashMap<String, JSONObject> httpPost(String endPoint, String token, List<Apk> apks) {
+        HashMap<String, JSONObject> result = new HashMap<String, JSONObject>() 
+        for(Apk apk in apks) {
+            HttpClient httpclient = new DefaultHttpClient()
+            HttpPost httppost = new HttpPost(endPoint)
+            MultipartEntity request_entity = new MultipartEntity()
+            Charset charset = Charset.forName(HTTP.UTF_8)
 
-        if (entity != null) {
-            InputStream is = entity.getContent()
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is))
-            JSONObject json = new JSONObject(reader.readLine())
-            return json
-            try {
-            } finally {
-              is.close()
+            File file = apk.file
+            request_entity.addPart("file", new FileBody(file.getAbsoluteFile()))
+            request_entity.addPart("token", new StringBody(token, charset))
+
+            HashMap<String, String> params = apk.getParams() 
+            for (String key : params.keySet()) {
+                request_entity.addPart(key, new StringBody(params.get(key), charset))
+            }
+
+            httppost.setEntity(request_entity)
+            HttpResponse response = httpclient.execute(httppost)
+            HttpEntity entity = response.getEntity()
+
+            if (entity != null) {
+                InputStream is = entity.getContent()
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is))
+                JSONObject json = new JSONObject(reader.readLine())
+                result.put(apk.name, json)
+                try {
+                } finally {
+                    is.close()
+                }
             }
         }
-        return null
+        return result 
     }
-}
-
-class DeployGateExtension {
-    String apkPath
-    String userName
-    String token
-    String message 
-    String distributionKey
-    String releaseNote
 }
