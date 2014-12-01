7964d085dfe52e3fb9e72462e6f257c4a1cc89e5
henteko <henteko07@gmail.com>
2014-12-01 13:43:13 +0900

    add project test

--- src/test/groovy/com/deploygate/gradle/plugins/ApkTest.groovy
+++ src/test/groovy/com/deploygate/gradle/plugins/ApkTest.groovy
@@ -1,5 +1,6 @@
 package com.deploygate.gradle.plugins
 
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
 import static org.junit.Assert.assertTrue
@@ -34,6 +35,36 @@ class ApkTest {
         Apk apk = new Apk(name, file)
         checkApk(apk, name, file, message, distributionKey, releaseNote, visibility)
         checkParams(apk, message, distributionKey, releaseNote, visibility)
+    }
+
+    @Test
+    public void projectTest() {
+        def project = new ProjectBuilder().withProjectDir(new File('src/test/project')).build()
+        project.apply plugin: 'deploygate'
+        project.deploygate {}
+        project.evaluate()
+
+        def apks = Apk.getApks(project)
+
+        String testApkName = "Test"
+        File testApkFile = null
+        String testApkMessage = "test message"
+        String testApkDistributionKey = "key"
+        String testApkReleaseNote = "release note"
+        String testApkVisibility = "private"
+        Apk testApk = apks.get(0)
+        checkApk(testApk, testApkName, testApkFile, testApkMessage, testApkDistributionKey, testApkReleaseNote, testApkVisibility)
+        checkParams(testApk, testApkMessage, testApkDistributionKey, testApkReleaseNote, testApkVisibility)
+
+        String test2ApkName = "Test2"
+        File test2ApkFile = null
+        String test2ApkMessage = ""
+        String test2ApkDistributionKey = null
+        String test2ApkReleaseNote = null
+        String test2ApkVisibility = "public"
+        Apk test2Apk = apks.get(1)
+        checkApk(test2Apk, test2ApkName, test2ApkFile, test2ApkMessage, test2ApkDistributionKey, test2ApkReleaseNote, test2ApkVisibility)
+        checkParams(test2Apk, test2ApkMessage, test2ApkDistributionKey, test2ApkReleaseNote, test2ApkVisibility)
     }
 
     public void checkApk(Apk apk, String name, File file, String message, String distributionKey, String releaseNote, String visibility) {
