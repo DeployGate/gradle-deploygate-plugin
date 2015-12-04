490761b2a5b55ec929ceb0be640e72dccb346091
henteko <henteko07@gmail.com>
2015-12-05 04:51:08 +0900

    fix before and after deploy

--- release.sh
+++ release.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build uploadArchives -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -PsonatypeUsername=henteko -PsonatypeFullname=deploygate -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -PsonatypePassword=$SONATYPE_PASSWORD
-curl -X POST --data-urlencode 'payload={"channel": "#henteko-test", "username": "DG-RELEASE-GRADLE-PLUGIN", "text": "Released a gradle-deploygate-plugin in Maven Central! Please check <https://oss.sonatype.org/content/groups/public/com/deploygate/|public> or <https://oss.sonatype.org/index.html#stagingRepositories|stagingRepositories>", "icon_emoji": ":tada:"}' $SLACK_URL
