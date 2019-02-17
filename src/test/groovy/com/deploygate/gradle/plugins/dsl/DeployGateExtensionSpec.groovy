f6bd5c5a47f4c84ff6567131a06836cb8b39a4ff
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-02-18 02:15:40 +0900

    Added a spec for DeployGate Extension

---
+++ src/test/groovy/com/deploygate/gradle/plugins/dsl/DeployGateExtensionSpec.groovy
@@ -0,0 +1,91 @@
+package com.deploygate.gradle.plugins.dsl
+
+import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class DeployGateExtensionSpec extends Specification {
+    @Rule
+    TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    File buildGradle
+
+    def setup() {
+        buildGradle = testProjectDir.newFile("build.gradle")
+    }
+
+    def "can accept a given extension"() {
+        given:
+        Project project = ProjectBuilder.builder().withProjectDir(testProjectDir.root).build()
+        testProjectDir.newFile("settings.gradle") << "rootProject.name = 'ok'"
+
+        and:
+        def buildFileContent = """
+deploygate {
+  appOwnerName = "user1"
+  apiToken = "token1"
+  deployments {
+    dep1 {
+    }
+    dep2 {
+      sourceFile = "build.gradle" as File
+      distributionKey = "distKey"
+      releaseNote = "note1"
+      uploadMessage = "message1"
+      skipAssemble = true
+    }
+    backward {
+      message = "backward message"
+      noAssemble = true
+    }
+  }
+}
+"""
+
+        buildGradle.exists() && buildGradle.delete() && buildGradle.createNewFile()
+        buildGradle << buildFileContent
+
+        and:
+        NamedDomainObjectContainer<VariantBasedDeployTarget> targets = project.container(VariantBasedDeployTarget)
+        project.extensions.add("deploygate", new DeployGateExtension(project, targets))
+        project.evaluate()
+
+        when:
+        def result = project.deploygate as DeployGateExtension
+
+        then:
+        result.appOwnerName == "user1"
+        result.apiToken == "token1"
+        result.deployments*.name.sort() == ["dep1", "dep2", "backward"].sort()
+
+        when:
+        def dep1 = result.deployments.findByName("dep1")
+
+        then:
+        dep1.sourceFile == null
+        dep1.uploadMessage == null
+        dep1.releaseNote == null
+        dep1.distributionKey == null
+        !dep1.skipAssemble
+
+        when:
+        def dep2 = result.deployments.findByName("dep2")
+
+        then:
+        dep2.sourceFile.name == "build.gradle"
+        dep2.uploadMessage == "message1"
+        dep2.releaseNote == "note1"
+        dep2.distributionKey == "distKey"
+        dep2.skipAssemble
+
+        when:
+        def backward = result.deployments.findByName("backward")
+
+        then:
+        backward.uploadMessage == "backward message"
+        backward.skipAssemble
+    }
+}
