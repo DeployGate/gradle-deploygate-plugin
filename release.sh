232f859057477edce29f479f7bdfefdbe86e8bda
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-02-22 15:00:39 +0900

    Added version and tag verification

--- release.sh
+++ release.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+set -eu
+
+if [[ "$(cat VERSION)" != "${TRAVIS_TAG:-}" ]]; then
+  echo "tag and version verification failed" 1>&2
+  echo "Required tag was $(cat VERSION) but ${TRAVIS_TAG:-not found} had come." 1>&2
+  exit 1
+fi
+
 ./gradlew clean build bintrayUpload
