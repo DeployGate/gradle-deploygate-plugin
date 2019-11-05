579a962bf2d4d482bf2a8e1df001e37e43ba978d
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-11-05 11:07:47 +0900

    Update AcceptanceTestSpec.groovy

--- src/test/acceptance/com/deploygate/gradle/plugins/AcceptanceTestSpec.groovy
+++ src/test/acceptance/com/deploygate/gradle/plugins/AcceptanceTestSpec.groovy
@@ -15,8 +15,8 @@ class AcceptanceTestSpec extends AcceptanceTestBaseSpec {
                 new AGPEnv("3.2.0", "4.6"),
                 new AGPEnv("3.3.2", "4.10.1"),
                 new AGPEnv("3.4.1", "5.1.1"),
-                new AGPEnv("3.5.1", "5.4.1"),
-                new AGPEnv("3.6.0-beta01", "5.6.1"),
+                new AGPEnv("3.5.2", "5.4.1"),
+                new AGPEnv("3.6.0-beta03", "5.6.1"),
         ]
     }
 }
