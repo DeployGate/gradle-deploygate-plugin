a896ef0269463dfc4a0a8a134b57848594eeff39
Jumpei Matsuda <jmatsu.drm@gmail.com>
2020-10-14 14:57:19 +0900

    Use 4.1.0 to test

--- src/test/unrollAcceptance/com/deploygate/gradle/plugins/internal/agp/AndroidGradlePluginAcceptanceSpec.groovy
+++ src/test/unrollAcceptance/com/deploygate/gradle/plugins/internal/agp/AndroidGradlePluginAcceptanceSpec.groovy
@@ -67,7 +67,7 @@ class AndroidGradlePluginAcceptanceSpec extends Specification {
         "3.5.0"          | "5.4.1"
         "3.6.0"          | "5.6.4"
         "4.0.0"          | "6.1.1"
-        "4.1.0-beta01"   | "6.5-rc-1"
+        "4.1.0"          | "6.5"
         "4.2.0-alpha01"  | "6.5-rc-1"
     }
 }
