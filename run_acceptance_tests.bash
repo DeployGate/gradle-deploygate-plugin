09c3ee72bcf960850ccce630a6c284ca56e86b92
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-01-11 16:45:29 +0900

    feat: Added AGP 7.1-rc01 and 7.2-alpha07 test-cases

--- run_acceptance_tests.bash
+++ run_acceptance_tests.bash
@@ -5,7 +5,7 @@ set -uxo pipefail
 
 ./gradlew testUnrollAcceptanceTest
 
-while read TEST_AGP_VERSION TEST_GRADLE_VERSION; do
+while read -r TEST_AGP_VERSION TEST_GRADLE_VERSION; do
   export TEST_AGP_VERSION TEST_GRADLE_VERSION
   ./gradlew testPluginAcceptanceTest
 done < <(cat<<EOF
@@ -16,5 +16,7 @@ done < <(cat<<EOF
 4.2.0 6.7.1
 4.2.0 7.0.2
 7.0.0 7.0.2
+7.1.0-rc01 7.1.1
+7.2.0-alpha07 7.2
 EOF
 )
