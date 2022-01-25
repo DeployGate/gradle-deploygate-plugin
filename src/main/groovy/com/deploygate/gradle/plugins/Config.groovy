cc73cb98eba8cb412b6935d1d51b956189f5c0c1
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-01-25 13:39:17 +0900

    feat: Set client-id and version-name

--- src/main/groovy/com/deploygate/gradle/plugins/Config.groovy
+++ src/main/groovy/com/deploygate/gradle/plugins/Config.groovy
@@ -1,9 +1,20 @@
 package com.deploygate.gradle.plugins
 
 class Config {
-    static final def VERSION = Config.class.getResourceAsStream("/VERSION").text.trim()
-    static final def USER_AGENT = "gradle-deploygate-plugin/${VERSION}"
     static final def DEPLOYGATE_ROOT = 'https://deploygate.com'
+    static final def VERSION
+    static final def VERSION_NAME
+
+    static {
+        VERSION = Config.class.getResourceAsStream("/VERSION").text.trim()
+        def version_name_stream = Config.class.getResourceAsStream("/VERSION_NAME")
+
+        if (version_name_stream != null) {
+            VERSION_NAME = version_name_stream.text.trim()
+        } else {
+            VERSION_NAME = "unavailable"
+        }
+    }
 
     static boolean shouldOpenAppDetailAfterUpload() {
         return System.getenv(DeployGatePlugin.ENV_NAME_OPEN_APP_DETAIL_AFTER_UPLOAD)
