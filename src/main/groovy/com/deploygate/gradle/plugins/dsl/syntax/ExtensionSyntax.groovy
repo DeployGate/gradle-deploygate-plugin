f466f2489905f3fd8aa272f69a22284e707de89d
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-10-24 02:03:14 +0900

    feat: Change the syntax to accept Kotlin DSL friendly syntax

--- src/main/groovy/com/deploygate/gradle/plugins/dsl/syntax/ExtensionSyntax.groovy
+++ src/main/groovy/com/deploygate/gradle/plugins/dsl/syntax/ExtensionSyntax.groovy
@@ -6,20 +6,32 @@ import org.gradle.api.NamedDomainObjectContainer
 import javax.annotation.Nonnull
 
 interface ExtensionSyntax {
+    /**
+     * Set the api token.
+     *
+     * @param apiToken
+     * @see com.deploygate.gradle.plugins.DeployGatePlugin#ENV_NAME_API_TOKEN
+     */
     void setApiToken(@Nonnull String apiToken)
 
+    /**
+     * Set the application owner name.
+     *
+     * @param appOwnerName
+     * @see com.deploygate.gradle.plugins.DeployGatePlugin#ENV_NAME_APP_OWNER_NAME
+     */
     void setAppOwnerName(@Nonnull String appOwnerName)
 
     /**
-     * this is for Groovy configuration
+     * Define deployments for each product flavors.
+     *
      * @param closure
      */
-    void deployments(Closure closure)
+    void deployments(Closure closure) // Groovy
 
     /**
-     * this is for Kotlin-DSL
-     * @param closure
+     * Define deployments for each product flavors.
      */
     @Nonnull
-    NamedDomainObjectContainer<NamedDeployment> getDeployments()
+    NamedDomainObjectContainer<NamedDeployment> getDeployments() // for Kotlin DSL
 }
