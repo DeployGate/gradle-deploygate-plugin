a39beddade1d4e75d20089784b3dedeac1612dcb
Jumpei Matsuda <jmatsu.drm@gmail.com>
2023-02-16 11:20:21 +0900

    refactor: Don't use Task#project to avoid configruation cache enabled builds's constraints

--- src/main/groovy/com/deploygate/gradle/plugins/tasks/LoginTask.groovy
+++ src/main/groovy/com/deploygate/gradle/plugins/tasks/LoginTask.groovy
@@ -1,94 +1,101 @@
 package com.deploygate.gradle.plugins.tasks
 
-import com.deploygate.gradle.plugins.DeployGatePlugin
 import com.deploygate.gradle.plugins.credentials.CliCredentialStore
+import com.deploygate.gradle.plugins.dsl.DeployGateExtension
 import com.deploygate.gradle.plugins.internal.http.ApiClient
+import com.deploygate.gradle.plugins.internal.http.GetCredentialsResponse
 import com.deploygate.gradle.plugins.utils.BrowserUtils
 import com.deploygate.gradle.plugins.utils.UrlUtils
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpServer
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
 class LoginTask extends DefaultTask {
-    @Internal def port = 0
 
-    @Internal CountDownLatch latch
-    boolean saved
-    @Internal CliCredentialStore localCredential
+    @Internal
+    DeployGateExtension deployGateExtension
+
+    private CountDownLatch latch
+
+    @NotNull
+    private CliCredentialStore localCredential
+
+    @Nullable
+    private String onetimeKey
 
     @TaskAction
     def setup() {
         localCredential = new CliCredentialStore()
-        if (!hasCredential())
-            if (!setupCredential())
-                throw new RuntimeException('Failed to retrieve DeployGate credentials. Please try again or specify it in your build.gradle script.')
 
-        project.deploygate.appOwnerName =
-                [project.deploygate.appOwnerName, System.getenv(DeployGatePlugin.ENV_NAME_APP_OWNER_NAME), System.getenv(DeployGatePlugin.ENV_NAME_APP_OWNER_NAME_V1), localCredential.name].find {
-                    it != null
-                }
-        project.deploygate.apiToken =
-                [project.deploygate.apiToken, System.getenv(DeployGatePlugin.ENV_NAME_API_TOKEN), localCredential.token].find {
-                    it != null
-                }
+        if (!(deployGateExtension.appOwnerName && deployGateExtension.apiToken)) {
+            if (!setupCredential()) {
+                throw new RuntimeException('We could not retrieve DeployGate credentials. Please make sure you have configured app owner name and api token or any browser application is available to launch the authentication flow.')
+            }
+
+            println "Welcome ${localCredential.name}!"
+
+            logger.info("The authentication has succeeded. The application owner name is ${localCredential.name}.")
+
+            // We should be carefully handling these values to avoid the inconsistent credentials and non-idempotency.
+            if (deployGateExtension.appOwnerName && deployGateExtension.appOwnerName != localCredential.name) {
+                logger.error("Another application owner name (${deployGateExtension.appOwnerName}) has already been configured")
+                throw new GradleException("The authentication has succeeded but the configured application owner name conflicts with the credentials that currently retrieved.")
+            } else if (deployGateExtension.apiToken && deployGateExtension.apiToken != localCredential.token) {
+                throw new GradleException("The authentication has succeeded but the configured api token doesn't match with the credentials that currently retrieved")
+            }
+
+            deployGateExtension.setAppOwnerName(localCredential.name)
+            deployGateExtension.setApiToken(localCredential.token)
+        }
     }
 
-    boolean hasCredential() {
-        hasCreadentialInScript() || hasSavedCredential() || hasCredentialInEnv()
-    }
-
-    boolean hasCredentialInEnv() {
-        [System.getenv(DeployGatePlugin.ENV_NAME_APP_OWNER_NAME), System.getenv(DeployGatePlugin.ENV_NAME_APP_OWNER_NAME_V1)].any() &&
-                [System.getenv(DeployGatePlugin.ENV_NAME_API_TOKEN)].any()
-    }
-
-    // From Gradle 7.0, only one method to get boolean property value is allowed
-    // whereas Groovy generates both isSaved() and getSaved(), Gradle 7.0 considers 2 getters exist for saved property.
-    // To suppress getSaved() we explicitly define isSaved().
-    private boolean isSaved() {
-        saved
-    }
-
-    private boolean hasSavedCredential() {
-        localCredential.name && localCredential.token
-    }
-
-    private boolean hasCreadentialInScript() {
-        project.deploygate.appOwnerName && project.deploygate.apiToken
-    }
-
-    def setupCredential() {
-        saved = false
+    /**
+     * Launch the authentication flow and fetch the credentials if possible
+     * @return true if the credentials are persisted, otherwise false.
+     */
+    private boolean setupCredential() {
         if (BrowserUtils.hasBrowser()) {
             setupBrowser()
         } else {
             setupTerminal()
         }
+
+        if (!onetimeKey || !retrieveCredentialFromKey(onetimeKey)) {
+            return false
+        }
+
+        deployGateExtension.notifyKey = onetimeKey
+        deployGateExtension.notifyServer('credential_saved')
+
+        return true
     }
 
-    def setupTerminal() {
-        // @TODO implement
+    private void setupTerminal() {
+        logger.error("The authentication flow within the terminal is not supported yet.")
+        // @TODO implement the terminal authentication flow
         false
     }
 
-    def setupBrowser() {
+    /**
+     * Launch the authentication flow by using a browser application.
+     */
+    private void setupBrowser() {
         def server
         try {
             server = startLocalServer()
-            openBrowser()
+            openBrowser(server.address.port)
             waitForResponse()
-            if (saved) {
-                println "Welcome ${localCredential.name}!"
-                return true
-            }
         } catch (e) {
-            logger.error("Failed to log in with browser: " + e.message)
+            logger.error("Failed to log in with browser: ${e.message}", e)
         } finally {
             if (server) {
                 server.stop(1)
@@ -96,33 +103,35 @@ class LoginTask extends DefaultTask {
         }
     }
 
-    boolean openBrowser() {
-        def url = "${project.deploygate.endpoint}/cli/login?port=${port}&client=gradle"
-        if (BrowserUtils.openBrowser(url))
-            return true
-        logger.warn 'Could not open a browser on current environment.'
-        println 'Please log in to DeployGate by opening the following URL on your browser:'
-        println url
-        false
+    void openBrowser(int port) {
+        def url = "${deployGateExtension.endpoint}/cli/login?port=${port}&client=gradle"
+
+        if (!BrowserUtils.openBrowser(url)) {
+            logger.error('Could not open a browser on current environment.')
+            println 'Please log in to DeployGate by opening the following URL on your browser:'
+            println url
+        }
     }
 
     def startLocalServer() {
+        latch = new CountDownLatch(1)
+
         def address = new InetSocketAddress("localhost", 0)
         def httpServer = HttpServer.create(address, 0)
-        port = httpServer.address.port
 
         httpServer.createContext "/token", { HttpExchange httpExchange ->
-            def query = UrlUtils.parseQueryString(httpExchange.requestURI.query)
-            project.deploygate.notifyKey = query.key
-            httpExchange.sendResponseHeaders(204, -1)
-            httpExchange.close()
+            try {
+                httpExchange.sendResponseHeaders(204, -1)
+                httpExchange.close()
 
-            if (!query.containsKey('cancel')) {
-                retrieveCredentialFromKey(query.key)
-                project.deploygate.notifyServer 'credential_saved'
+                def query = UrlUtils.parseQueryString(httpExchange.requestURI.query)
+
+                if (!query.containsKey('cancel')) {
+                    onetimeKey = query.key
+                }
+            } finally {
+                latch.countDown()
             }
-
-            latch.countDown()
         }
         httpServer.start()
         httpServer
@@ -133,24 +142,29 @@ class LoginTask extends DefaultTask {
         def timeout = 180 * 1000
         def start = System.currentTimeMillis()
 
-        latch = new CountDownLatch(1)
-        while (!latch.await(5000, TimeUnit.MILLISECONDS)) {
+        while (!latch.await(5, TimeUnit.SECONDS)) {
             print "."
-            if (System.currentTimeMillis() - start > timeout)
+
+            if (System.currentTimeMillis() - start > timeout) {
                 throw new TimeoutException('Timeout while waiting for browser response')
+            }
         }
     }
 
     boolean retrieveCredentialFromKey(String key) {
-        try {
-            def response = ApiClient.instance.getCredentials(key)
+        ApiClient.Response<GetCredentialsResponse> response
 
-            if (localCredential.saveLocalCredentialFile(response.rawResponse)) {
-                localCredential.load()
-                saved = true
-            }
+        try {
+            response = ApiClient.instance.getCredentials(key)
         } catch (Throwable th) {
             logger.error('failed to retrieve credential', th)
+            return false
         }
+
+        if (!localCredential.saveLocalCredentialFile(response.rawResponse)) {
+            throw new GradleException("failed to save the fetched credentials")
+        }
+
+        return localCredential.load()
     }
 }
