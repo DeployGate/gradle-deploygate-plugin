fefd74ab7543235045db35415d62a18066b5bc1c
Jumpei Matsuda <jmatsu.drm@gmail.com>
2023-03-10 13:59:52 +0900

    refactor: Remove unsued classes and references

--- src/main/groovy/com/deploygate/gradle/plugins/tasks/factory/DeployGateTaskFactory.groovy
+++ src/main/groovy/com/deploygate/gradle/plugins/tasks/factory/DeployGateTaskFactory.groovy
@@ -1,13 +1,10 @@
 package com.deploygate.gradle.plugins.tasks.factory
 
-import com.deploygate.gradle.plugins.dsl.DeployGateExtension
-import com.deploygate.gradle.plugins.internal.gradle.TaskFactory
 import com.deploygate.gradle.plugins.tasks.Constants
-import org.gradle.api.Project
 
 import javax.annotation.Nonnull
 
-abstract class DeployGateTaskFactory {
+final class DeployGateTaskFactory {
     public static final String GROUP_NAME = Constants.TASK_GROUP_NAME
     public static String SUFFIX_APK_TASK_NAME = "uploadDeployGate"
     public static String SUFFIX_AAB_TASK_NAME = "uploadDeployGateAab"
@@ -20,24 +17,5 @@ abstract class DeployGateTaskFactory {
     @Nonnull
     static String uploadAabTaskName(@Nonnull String variantName) {
         return "$SUFFIX_AAB_TASK_NAME${variantName.capitalize()}"
-    }
-
-    @Nonnull
-    final Project project
-    @Nonnull
-    final TaskFactory taskFactory
-
-    DeployGateTaskFactory(@Nonnull Project project) {
-        this.project = project
-        this.taskFactory = new TaskFactory(project)
-    }
-
-    private Closure<DeployGateExtension> deployGateExtensionClosure = { ->
-        project.deploygate
-    }.memoize()
-
-    @Nonnull
-    DeployGateExtension getDeployGateExtension() {
-        return deployGateExtensionClosure.call()
     }
 }
