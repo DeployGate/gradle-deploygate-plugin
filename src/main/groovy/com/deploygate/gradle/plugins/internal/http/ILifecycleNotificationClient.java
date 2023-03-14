b931fa27b76d1c3ea31041e07b011b1b93a04137
Jumpei Matsuda <jmatsu.drm@gmail.com>
2023-03-14 10:45:01 +0900

    feat: Use null factory pattern for lifecycle notification client

---
+++ src/main/groovy/com/deploygate/gradle/plugins/internal/http/ILifecycleNotificationClient.java
@@ -0,0 +1,24 @@
+package com.deploygate.gradle.plugins.internal.http;
+
+import org.apache.hc.client5.http.HttpResponseException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface ILifecycleNotificationClient {
+    /**
+     * Get the credentials from the server
+     *
+     * @throws HttpResponseException is thrown if a request is an error inclduing 4xx and 5xx
+     * @throws NetworkFailure        is thrown if a network trouble happens
+     */
+    @Nullable
+    HttpClient.Response<GetCredentialsResponse> getCredentials() throws HttpResponseException, NetworkFailure;
+
+    boolean notifyOnCredentialSaved();
+
+    boolean notifyOnBeforeArtifactUpload(long fileSize);
+
+    boolean notifyOnSuccessOfArtifactUpload(@NotNull String appDetailPath);
+
+    boolean notifyOnFailureOfArtifactUpload(@NotNull String errorMessage);
+}
--- src/main/groovy/com/deploygate/gradle/plugins/internal/http/LifecycleNotificationClient.java
+++ src/main/groovy/com/deploygate/gradle/plugins/internal/http/LifecycleNotificationClient.java
@@ -8,14 +8,14 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 
-public class LifecycleNotificationClient {
+public class LifecycleNotificationClient implements ILifecycleNotificationClient {
     @NotNull
     private final HttpClient httpClient;
     @NotNull
     private final String notifyKey;
 
     /**
-     * @param httpClient
+     * @param httpClient a real http client
      * @param notifyKey  a notification key. In general, this is generated in the authentication flow.
      */
     LifecycleNotificationClient(@NotNull HttpClient httpClient, @NotNull String notifyKey) {
@@ -23,31 +23,58 @@ public class LifecycleNotificationClient {
         this.notifyKey = notifyKey;
     }
 
-    /**
-     * Get the credentials from the server
-     *
-     * @throws HttpResponseException is thrown if a request is an error inclduing 4xx and 5xx
-     * @throws NetworkFailure        is thrown if a network trouble happens
-     */
     @NotNull
+    @Override
     public HttpClient.Response<GetCredentialsResponse> getCredentials() throws HttpResponseException, NetworkFailure {
         HttpUriRequestBase requestBase = httpClient.buildRequest(HttpGet.METHOD_NAME, Collections.singletonMap("key", notifyKey), "cli", "credential");
 
         return httpClient.execute(requestBase, GetCredentialsResponse.class);
     }
 
+    @Override
+    public boolean notifyOnCredentialSaved() {
+        return notify(new NotifyActionRequest("credential_saved"));
+    }
+
+    @Override
+    public boolean notifyOnBeforeArtifactUpload(long fileSize) {
+        NotifyActionRequest request = new NotifyActionRequest("start_upload");
+        request.setParameter("length", Long.toString(fileSize));
+        return notify(request);
+    }
+
+    @Override
+    public boolean notifyOnSuccessOfArtifactUpload(@NotNull String appDetailPath) {
+        NotifyActionRequest request = new NotifyActionRequest("'upload_finished'");
+        request.setParameter("path", appDetailPath);
+        return notify(request);
+    }
+
+    @Override
+    public boolean notifyOnFailureOfArtifactUpload(@NotNull String errorMessage) {
+        NotifyActionRequest request = new NotifyActionRequest("'upload_finished'");
+        request.setParameter("error", "true");
+        request.setParameter("message", errorMessage);
+        return notify(request);
+    }
+
     /**
      * Notify the plugin event to the server
      *
      * @param request a request to the server that must contain an action name
+     * @return true anyway
      * @throws HttpResponseException is thrown if a request is an error inclduing 4xx and 5xx
      * @throws NetworkFailure        is thrown if a network trouble happens
      */
-    @SuppressWarnings("RedundantThrows")
-    public void notify(@NotNull NotifyActionRequest request) throws HttpResponseException, NetworkFailure {
-        HttpUriRequestBase httpPost = httpClient.buildRequest(HttpPost.METHOD_NAME, "cli", "notify");
-        httpPost.setEntity(request.toEntity(notifyKey));
+    private boolean notify(@NotNull NotifyActionRequest request) {
+        try {
+            HttpUriRequestBase httpPost = httpClient.buildRequest(HttpPost.METHOD_NAME, "cli", "notify");
+            httpPost.setEntity(request.toEntity(notifyKey));
 
-        httpClient.execute(httpPost, Object.class);
+            httpClient.execute(httpPost, Object.class);
+            return true;
+        } catch (Throwable ignore) {
+            return true; // returns true anyway!
+        }
     }
 }
