6b14d77434e4d8343a0dce1035ee29a258038ff2
Jumpei Matsuda <jmatsu.drm@gmail.com>
2024-01-12 17:14:28 +0900

    chore: Remove sqlite3 and other unused dependencies

--- docker/mock-server/config/application.rb
+++ docker/mock-server/config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
-require "rails/all"
+require "active_model/railtie" 
+require "action_controller/railtie"
+require "action_view/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- docker/mock-server/config/environments/production.rb
+++ docker/mock-server/config/environments/production.rb
@@ -30,9 +30,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -54,8 +51,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mock_server_production"
-
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -80,7 +75,4 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
