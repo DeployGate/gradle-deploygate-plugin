4685e703e26575337a06f657257c16f5ad5feede
Jumpei Matsuda <jmatsu.drm@gmail.com>
2024-10-16 16:49:52 +0900

    test: Read real env if no mock is found in test system env

--- src/test/groovy/com/deploygate/gradle/plugins/TestSystemEnv.groovy
+++ src/test/groovy/com/deploygate/gradle/plugins/TestSystemEnv.groovy
@@ -5,12 +5,16 @@ import org.junit.rules.ExternalResource
 class TestSystemEnv extends ExternalResource {
     private Map<String, Object> envStub = new HashMap<String, Object>()
 
-    def realEnv = System.getenv()
+    private def realEnv = System.getenv()
 
     @Override
     protected void before() throws Throwable {
         System.metaClass.static.getenv = { String name ->
-            envStub[name] as String
+            if (envStub.containsKey(name)) {
+                envStub[name] as String
+            } else {
+                realEnv[name]
+            }
         }
     }
 
